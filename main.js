8ad26145722dd0a379c90b39217968c2bcd52f14
Bijay <Bijaym095@gmail.com>
2023-01-06 18:05:30 +0545

    readme updated

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+//closing side navigation for mobile and tablet screens
 const closeBtn = document.querySelector("#close");
-console.log(closeBtn);
+
 closeBtn.addEventListener("click", () => {
   const linksContainer = document.querySelector("#navbarNavDropdown");
   linksContainer.classList.remove("show");
 });
+
+//making active scroll navigation
+window.addEventListener("scroll", () => {
+  const position = scrollY;
+
+  console.log(position);
+});
+
+console.log("hi");
